9000cbd9eb9860d5c17808b5b2b6a00c51992154
Douglas Oliveira <douglas13.augusto@hotmail.com>
2021-01-29 13:15:03 -0300

    first commit

---
+++ 4-ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto4");
+
+            double salario;
+            salario = 1200.50;
+
+            // int suporta 32 bits
+            int salarioEmInteiro;
+            salarioEmInteiro = (int)salario;
+
+            Console.WriteLine(salarioEmInteiro);
+
+            //o long suporta 64 bits
+            long idade;
+            idade = 13000000000;
+            Console.WriteLine(idade);
+
+            //o short suporta 16 bits
+            short quantidadeProdutos;
+            quantidadeProdutos = 150;
+            Console.WriteLine(quantidadeProdutos);
+
+            float altura = 1.80f;
+
+            Console.WriteLine(altura);
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 5 - CaracteresETextos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5___CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 5 - Caracteres e textos");
+
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura Cursos de tecnologia " + 2020;
+            string cursosProgramacao = 
+ @" - .NET +
+ - JAVA 
+ - JavaScript";
+            
+            Console.WriteLine(titulo);
+            Console.WriteLine(cursosProgramacao);
+
+
+                
+                
+                
+                
+            Console.ReadLine();
+
+        }
+    }
+}
